a58f0dd8556d1fda638955d34a4a1ca2886fe632
Maciej Kieżel <sasiusz@gmail.com>
2020-01-18 08:42:38 +0100

    dodanie rachunków w formacie PDF

--- django/sklep/myshop/orders/admin.py
+++ django/sklep/myshop/orders/admin.py
@@ -13,10 +13,16 @@ def order_detail(obj):
     return mark_safe('<a href="{}">Wyświetl</a>'.format(
         reverse('orders:admin_order_detail', args=[obj.id])))
 
+def order_pdf(obj):
+    return mark_safe('<a href="{}">PDF</a>'.format(
+        reverse('orders:admin_order_pdf', args=[obj.id])))
+order_pdf.allow_tags = True
+order_pdf.short_description = 'Rachunek PDF'
+
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated',order_detail]
+                    'created', 'updated',order_detail,order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
 
